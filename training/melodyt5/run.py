458b4e4567823c239ee0e08be824e64e64b467ea
ElectricAlexis <75775207+ElectricAlexis@users.noreply.github.com>
2025-01-29 17:01:59 +0800

    Delete training directory

--- training/melodyt5/run.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system('bash -c "source /root/miniconda3/etc/profile.d/conda.sh && conda activate melodyt5 && CUDA_VISIBLE_DEVICES=0 python train.py"')
-# os.system('bash -c "source /root/miniconda3/etc/profile.d/conda.sh && conda activate melodyt5 && python generate.py"')
